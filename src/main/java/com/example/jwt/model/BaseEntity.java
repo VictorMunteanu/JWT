a47c4772fdd30a468b4b1c5e8df05f02cd459ab9
Munteanu Victor <munteanuvictor46@gmail.com>
2021-10-17 16:45:07 +0300

    Created BaseEntity java class

---
+++ src/main/java/com/example/jwt/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.example.jwt.model;
+
+import lombok.Data;
+
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@MappedSuperclass
+@Data
+public class BaseEntity {
+
+    private long id;
+
+    private Date created;
+
+    private Date updated;
+}
