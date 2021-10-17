54f3240a95cc2e4a3257c2b7143e54a8ff4cf0a9
Munteanu Victor <munteanuvictor46@gmail.com>
2021-10-17 18:12:13 +0300

    Modify AuthenticationRestControllerV1

--- src/main/java/com/example/jwt/rest/AuthenticationRestControllerV1.java
+++ src/main/java/com/example/jwt/rest/AuthenticationRestControllerV1.java
@@ -1,10 +1,25 @@
 package com.example.jwt.rest;
 
+import com.example.jwt.security.jwt.JwtTokenProvider;
+import com.example.jwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/auth/")
 public class AuthenticationRestControllerV1 {
+    private final AuthenticationManager authenticationManager;
 
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final UserService userService;
+
+    @Autowired
+    public AuthenticationRestControllerV1(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
+    }
 }
