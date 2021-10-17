d13c98d48ba98fdf5d731bad9fe9f44a0989b173
Munteanu Victor <munteanuvictor46@gmail.com>
2021-10-17 18:10:00 +0300

    Create AuthenticationRestControllerV1

---
+++ src/main/java/com/example/jwt/rest/AuthenticationRestControllerV1.java
@@ -0,0 +1,10 @@
+package com.example.jwt.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/auth/")
+public class AuthenticationRestControllerV1 {
+
+}
