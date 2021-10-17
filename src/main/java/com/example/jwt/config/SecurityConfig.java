c607e2a144af2ef374b5742f2ff9923699316bea
Munteanu Victor <munteanuvictor46@gmail.com>
2021-10-17 18:04:28 +0300

    Create SecurityConfig

---
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.example.jwt.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
